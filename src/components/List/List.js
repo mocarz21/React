f947c1a5e30528321a1852a0a6b8911ff9a0c64b
Adam Mocarski <adammocarski22@gmail.com>
2022-07-12 22:21:38 +0200

    Add Components List Form ColumnForm Button Hero List SearchForm

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,29 @@
 import styles from './List.module.scss';
-import Column from '../Column/Column'
+import Column from '../Column/Column';
+import { useState } from 'react';
+//import { useEffect } from 'react';
+import shortid from 'shortid';
+import ColumnForm from '../ColumnForm/ColumnForm'
+
 const List = () =>{
+    
+    const [columns, setColumns] = useState([
+        {id: 1, title: 'Books', icon: 'book'},
+        {id: 2, title: 'Movies', icon: 'film'},
+        {id: 3, title: 'Games Test', icon: 'gamepad'}
+    ]);
+    // useEffect(() =>{
+
+    //     setTimeout(() =>{
+    //         setColumns([...columns, {id:4, title:'test column'}]);
+    //     }, 2000);
+    // }, []);
+
+    const addColumn = newColumn => {
+        setColumns([...columns, { id: shortid(), title: newColumn.title}]);
+    };
+
+
     return(
         <div>
             <header className={styles.header}>
@@ -8,10 +31,9 @@ const List = () =>{
             </header>
             <p className={styles.description}>interesting thinghs i want to check out!</p>
             <section className={styles.columns}>
-                <Column title="Books" icon="book"/>
-                <Column title="Movies" icon="gamepad"/>
-                <Column title="Games" icon="film"/>
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon}/>)}
             </section>
+            <ColumnForm action={addColumn}/>
         </div>
     )
 }
